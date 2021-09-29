53e3ad6d5bad28e1ad12e1a3bd3d3e660586acae
Naman Goyal <namangoyal1001@gmail.com>
2021-09-28 23:26:13 -0400

    Update ice cream generation logic

--- generate_types.py
+++ generate_types.py
@@ -9,12 +9,10 @@ flavors = list(range(1,13))
 l = 24 # cols
 w = 15 # rows
 h = 8 # height
-container = np.empty((l, w, h), dtype=np.int)
+empty_container = np.empty((l, w, h), dtype=np.int)
 
-assert container.size % len(flavors) == 0, "Number of flavors can't divide container size"
-flavor_assigned = np.repeat(np.array(flavors, dtype=np.int), container.size/ len(flavors))
-
-rng.shuffle(flavor_assigned)
+assert empty_container.size % len(flavors) == 0, "Number of flavors can't divide container size"
+flavor_repeated = np.repeat(np.array(flavors, dtype=np.int), empty_container.size/ len(flavors))
 
 dirname = "types"
 
@@ -24,5 +22,5 @@ os.makedirs(dirname)
 
 for ice_cream_type in [2,3,4,5,6,8,9,10,12]:
     with open(os.path.join(dirname, "{}.json".format(ice_cream_type)),"w") as jf:
-        rng.shuffle(flavor_assigned)
-        json.dump(np.reshape(flavor_assigned, (l, w, h)).tolist(), jf)
+        flavor_assigned = rng.permutation(flavor_repeated)
+        json.dump(np.reshape(flavor_assigned, empty_container.shape).tolist(), jf)
