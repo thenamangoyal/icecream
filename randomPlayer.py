5f512c05cfd76d7e23aeddc96f88f380b4d72425
Naman Goyal <namangoyal1001@gmail.com>
2021-10-07 19:11:54 -0400

    Add typing hints

--- randomPlayer.py
+++ randomPlayer.py
@@ -2,14 +2,14 @@ import math
 import numpy as np
 import copy
 import logging
-from typing import Dict, List
+from typing import Callable, Dict, List, Tuple, Union
 
 class Player:
-    def __init__(self, flavor_preference: List, rng: np.random.Generator, logger: logging.Logger) -> None:
+    def __init__(self, flavor_preference: List[int], rng: np.random.Generator, logger: logging.Logger) -> None:
         """Initialise the player with given preference.
 
         Args:
-            flavor_preference (List): flavor preference, least flavored flavor is first element in the list and most flavored is most preferred
+            flavor_preference (List[int]): flavor preference, least flavored flavor is first element in the list and most flavored is most preferred
             rng (np.random.Generator): numpy random number generator, use this for same player behvior across run
             logger (logging.Logger): logger use this like logger.info("message")
         """
@@ -18,7 +18,7 @@ class Player:
         self.logger = logger
         self.state = None
     
-    def serve(self, top_layer: np.ndarray, curr_level: np.ndarray, player_idx: int, get_flavors, get_player_count, get_served, get_turns_received) -> Dict:
+    def serve(self, top_layer: np.ndarray, curr_level: np.ndarray, player_idx: int, get_flavors: Callable[[], List], get_player_count: Callable[[], int], get_served: Callable[[], List[Dict[int, int]]], get_turns_received: Callable[[], List[int]]) -> Dict[str, Union[Tuple[int],int]]:
         x = self.rng.random()
         if x < 0.95:
             i = self.rng.integers(0, top_layer.shape[0]-1)
