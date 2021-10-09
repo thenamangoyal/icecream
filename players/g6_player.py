d5d6497467d9c02f8889f457c1d82718f48d6d8b
baium <yb2519@columbia.edu>
2021-10-09 01:48:57 -0400

    "finished greedy"

--- players/g6_player.py
+++ players/g6_player.py
@@ -3,6 +3,7 @@ import numpy as np
 import copy
 import logging
 from typing import Callable, Dict, List, Tuple, Union
+from .g6_greedy import greedy,get_level,get_icecream
 
 
 class Player:
@@ -17,11 +18,11 @@ class Player:
         self.flavor_preference = flavor_preference
         self.rng = rng
         self.logger = logger
-        self.state = None
+        self.state = {"count":0}
+        self.greedy = greedy
 
     def serve(self, top_layer: np.ndarray, curr_level: np.ndarray, player_idx: int, get_flavors: Callable[[], List[int]], get_player_count: Callable[[], int], get_served: Callable[[], List[Dict[int, int]]], get_turns_received: Callable[[], List[int]]) -> Dict[str, Union[Tuple[int], int]]:
         """Request what to scoop or whom to pass in the given step of the turn. In each turn the simulator calls this serve function mulitple times for each step for a single player, until the player has scooped 24 units of ice-cream or asked to pass to next player or made an invalid request. If you have scooped 24 units of ice-cream in a turn then you get one last step in that turn where you can specify to pass to a player.
-
         Args:
             top_layer (np.ndarray): Numpy 2d array of size (24, 15) containing flavor at each cell location
             curr_level (np.ndarray): Numpy 2d array of size (24, 15) containing current level at each cell location from 8 to 0, where 8 is highest level at start and 0 means no icecream left at this level
@@ -30,23 +31,25 @@ class Player:
             get_player_count (Callable[[], int]): number of total players
             get_served (Callable[[], List[Dict[int, int]]]): returns a list of dictionaries corresponding to each player, each dictionary at index i tells how units of a flavor are present in the bowl of the player with index i. E.g. lets say the fourth element is {1: 0, 2: 8...} means the corresponding player with index 4 has 0 units of flavor 1 and 8 units of flavor
             get_turns_received (Callable[[], List[int]]): returns a list of integers corresponding to each player, each element at index i tells how many turns a player with index i has played so far.
-
         Returns:
             Dict[str, Union[Tuple[int],int]]: Return a dictionary of action to take in next step.
             2 possible return values
             {"action": "scoop",  "values" : (i,j)} stating to scoop the 4 cells with index (i,j), (i+1,j), (i,j+1), (i+1,j+1)
             {"action": "pass",  "values" : i} pass to next player with index i
         """
-        x = self.rng.random()
-        if x < 0.95:
-            i = self.rng.integers(0, top_layer.shape[0]-1)
-            j = self.rng.integers(0, top_layer.shape[1]-1)
+        action = None
+        values = None
+        if (self.state["count"]<24):
+            pos,amount = self.greedy(self, top_layer, curr_level, 24-self.state["count"])
             action = "scoop"
-            values = (i, j)
+            values = pos
+            self.state["count"]+=amount
         else:
-            other_player_list = list(range(0, get_player_count()))
-            other_player_list.remove(player_idx)
-            next_player = other_player_list[self.rng.integers(0, len(other_player_list))]
-            action = "pass"
-            values = next_player
+            self.state["count"] = 0
+            #self.passNextPlayer(self, top_layer: np.ndarray, curr_level: np.ndarray, player_idx: int, get_flavors:
+            #Callable[[], List[int]], get_player_count: Callable[[], int], get_served: Callable[
+            #    [], List[Dict[int, int]]], get_turns_received: Callable[[], List[int]])
+
         return {"action": action,  "values": values}
+
+
