41f42d25e1a59e8e37f8f1837a9c112d597bc219
Ivan Chau <ivanmchau@gmail.com>
2021-10-10 12:22:01 -0400

    greedy scoopy

--- players/g9_player.py
+++ players/g9_player.py
@@ -15,9 +15,27 @@ class Player:
             logger (logging.Logger): logger use this like logger.info("message")
         """
         self.flavor_preference = flavor_preference
+        self.flavor_preference_index = {}
+        for index, flavor in enumerate(self.flavor_preference):
+            self.flavor_preference_index[flavor] = len(self.flavor_preference) - index
+        self.flavor_preference_index[-1] = 0
         self.rng = rng
         self.logger = logger
-        self.state = None
+        self.state = {'remaining_moves':24}
+
+    def getGreedyScoop(self, top_layer: np.ndarray):
+        maxSquare = 0
+        maxCoords = None
+        getScore = lambda x, y: self.flavor_preference_index[top_layer[x][y]]
+        for i in range(top_layer.shape[0] - 1):
+            for j in range(top_layer.shape[1] - 1):
+                print(top_layer[i+1][j+1])
+                square = getScore(i,j) + getScore(i+1, j) + getScore(i+1, j+1) + getScore(i, j+1)
+                if square > maxSquare:
+                    maxSquare = square
+                    maxCoords = (i,j)
+        return maxCoords, maxSquare
+                
 
     def serve(self, top_layer: np.ndarray, curr_level: np.ndarray, player_idx: int, get_flavors: Callable[[], List[int]], get_player_count: Callable[[], int], get_served: Callable[[], List[Dict[int, int]]], get_turns_received: Callable[[], List[int]]) -> Dict[str, Union[Tuple[int], int]]:
         """Request what to scoop or whom to pass in the given step of the turn. In each turn the simulator calls this serve function multiple times for each step for a single player, until the player has scooped 24 units of ice-cream or asked to pass to next player or made an invalid request. If you have scooped 24 units of ice-cream in a turn then you get one last step in that turn where you can specify to pass to a player.
@@ -37,13 +55,22 @@ class Player:
             {"action": "scoop",  "values" : (i,j)} stating to scoop the 4 cells with index (i,j), (i+1,j), (i,j+1), (i+1,j+1)
             {"action": "pass",  "values" : i} pass to next player with index i
         """
-        x = self.rng.random()
-        if x < 0.95:
-            i = self.rng.integers(0, top_layer.shape[0]-1)
-            j = self.rng.integers(0, top_layer.shape[1]-1)
+        remaining_moves = self.state['remaining_moves']
+        if (remaining_moves > 0):
+            remaining_moves -= 1
             action = "scoop"
-            values = (i, j)
+            maxCoords, maxSquare = self.getGreedyScoop(top_layer)
+            values = maxCoords
+            '''
+            x = self.rng.random()
+            if x < 0.95:
+                i = self.rng.integers(0, top_layer.shape[0]-1)
+                j = self.rng.integers(0, top_layer.shape[1]-1)
+                action = "scoop"
+                values = (i, j)
+            '''
         else:
+            self.state['remaining_moves'] = 24
             other_player_list = list(range(0, get_player_count()))
             other_player_list.remove(player_idx)
             next_player = other_player_list[self.rng.integers(0, len(other_player_list))]
