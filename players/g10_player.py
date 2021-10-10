01dab22dc02bff383bdbf457a635ca4c419aa1df
Sam Raab <raabsm@gmail.com>
2021-10-10 15:46:05 -0400

    added greedy scooping

--- players/g10_player.py
+++ players/g10_player.py
@@ -18,6 +18,45 @@ class Player:
         self.rng = rng
         self.logger = logger
         self.state = None
+        self.curr_turn = 0
+        self.num_scoops_in_turn = 0
+
+
+    def calc_flavor_points(self, flavors_scooped, flavor_preference):
+        total = 0
+        for flavor_cell in flavors_scooped:
+            preference_idx = flavor_preference.index(flavor_cell)
+            preference_score = len(self.flavor_preference) - preference_idx
+            total += preference_score
+        return total
+
+
+    def calc_scoop_points(self, i, j, curr_level, top_layer, flavor_preference):
+        if i >= len(curr_level - 1) or j >= len(curr_level[0] - 1):
+            return 0
+        max_level = max(curr_level[i, j], curr_level[i, j+1], curr_level[i+1, j], curr_level[i+1, j+1])
+        flavor_cells = []
+        for i_offset in range(2):
+            for j_offset in range(2):
+                if curr_level[i + i_offset, j + j_offset] == max_level:
+                    flavor_cells.append(top_layer[i + i_offset, j + j_offset])
+        return self.calc_flavor_points(flavor_cells, flavor_preference)
+
+
+    def find_max_scoop(self, top_layer, curr_level, flavor_preference):
+        max_scoop_loc = (0, 0)
+        max_scoop_points = 0
+        for i in range(len(top_layer) - 1):
+            for j in range(len(top_layer[0]) - 1):
+                scoop_points = self.calc_scoop_points(i, j, curr_level, top_layer, flavor_preference)
+                if scoop_points > max_scoop_points:
+                    max_scoop_points = scoop_points
+                    max_scoop_loc = (i, j)
+
+        return max_scoop_loc
+
+
+
 
     def serve(self, top_layer: np.ndarray, curr_level: np.ndarray, player_idx: int, get_flavors: Callable[[], List[int]], get_player_count: Callable[[], int], get_served: Callable[[], List[Dict[int, int]]], get_turns_received: Callable[[], List[int]]) -> Dict[str, Union[Tuple[int], int]]:
         """Request what to scoop or whom to pass in the given step of the turn. In each turn the simulator calls this serve function multiple times for each step for a single player, until the player has scooped 24 units of ice-cream or asked to pass to next player or made an invalid request. If you have scooped 24 units of ice-cream in a turn then you get one last step in that turn where you can specify to pass to a player.
@@ -37,16 +76,22 @@ class Player:
             {"action": "scoop",  "values" : (i,j)} stating to scoop the 4 cells with index (i,j), (i+1,j), (i,j+1), (i+1,j+1)
             {"action": "pass",  "values" : i} pass to next player with index i
         """
-        x = self.rng.random()
-        if x < 0.95:
-            i = self.rng.integers(0, top_layer.shape[0]-1)
-            j = self.rng.integers(0, top_layer.shape[1]-1)
-            action = "scoop"
-            values = (i, j)
-        else:
-            other_player_list = list(range(0, get_player_count()))
-            other_player_list.remove(player_idx)
-            next_player = other_player_list[self.rng.integers(0, len(other_player_list))]
+        if get_turns_received()[player_idx] > self.curr_turn:
+            self.num_scoops_in_turn = 0
+            self.curr_turn = get_turns_received()[player_idx]
+
+
+        if self.num_scoops_in_turn >= 6:
+            ##changes with actual computation!
+
             action = "pass"
-            values = next_player
+            values = 0
+        else:
+            self.num_scoops_in_turn += 1
+
+            action = "scoop"
+            values = self.find_max_scoop(top_layer, curr_level, self.flavor_preference)
+
         return {"action": action,  "values": values}
+
+
