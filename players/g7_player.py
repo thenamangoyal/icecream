77f598f2ecb0535e80754acd8ab25a3fe6c34393
Kerem Guventurk <kg2900@columbia.edu>
2021-10-17 13:57:13 -0400

    merge with branch kerem

--- players/g7_player.py
+++ players/g7_player.py
@@ -9,7 +9,6 @@ import heapq
 class Player:
     def __init__(self, flavor_preference: List[int], rng: np.random.Generator, logger: logging.Logger) -> None:
         """Initialise the player with given preference.
-
         Args:
             flavor_preference (List[int]): flavor preference, most flavored flavor is first element in the list and last element is least preferred flavor
             rng (np.random.Generator): numpy random number generator, use this for same player behvior across run
@@ -37,6 +36,8 @@ class Player:
         self.total_cells = 2880
         self.distribution = self.get_init_distribution()
 
+        self.remaining_scoops = 24
+
     def get_init_distribution(self):
         flavor_preference_len = len(self.flavor_preference)
         return [self.total_cells / flavor_preference_len] * flavor_preference_len
@@ -53,12 +54,15 @@ class Player:
                 self.distribution[flavor - 1] -= consumed
 
     def update_hidden_cell_expectation(self, served, top_layer):
-        distribution = self.get_distribution(served, top_layer)
+        self.get_distribution(served, top_layer)
         total = sum(self.distribution)
-        ans = 0
-        for i, v in enumerate(self.distribution):
-            ans += self.flavor_points[i + 1] * max(v, 0) / total
-        self.flavor_points[-2] = ans
+        if total == 0:
+            self.flavor_points[-2] = 0
+        else:
+            ans = 0
+            for i, v in enumerate(self.distribution):
+                ans += self.flavor_points[i + 1] * max(v, 0) / total
+            self.flavor_points[-2] = ans
 
 
     def calculate_score_scoop(self, scoop):
@@ -127,7 +131,6 @@ class Player:
         num_unseen_cells = 0
 
         for level in range(max_level, min_level - 1, -1):
-            l = []
             for x in range(2):
                 for y in range(2):
                     if curr_level[i + x][j + y] == level:
@@ -141,11 +144,9 @@ class Player:
             score = (cumulative_score / num_scooped) if num_scooped > 0 else 0
             self.move_scores[-score] = (num_moves, num_scooped, i, j)
             prev_num_unseen_cells = num_unseen_cells
-            num_unseen_cells = 0
 
     def serve(self, top_layer: np.ndarray, curr_level: np.ndarray, player_idx: int, get_flavors: Callable[[], List[int]], get_player_count: Callable[[], int], get_served: Callable[[], List[Dict[int, int]]], get_turns_received: Callable[[], List[int]]) -> Dict[str, Union[Tuple[int], int]]:
         """Request what to scoop or whom to pass in the given step of the turn. In each turn the simulator calls this serve function multiple times for each step for a single player, until the player has scooped 24 units of ice-cream or asked to pass to next player or made an invalid request. If you have scooped 24 units of ice-cream in a turn then you get one last step in that turn where you can specify to pass to a player.
-
         Args:
             top_layer (np.ndarray): Numpy 2d array of size (24, 15) containing flavor at each cell location
             curr_level (np.ndarray): Numpy 2d array of size (24, 15) containing current level at each cell location from 8 to 0, where 8 is highest level at start and 0 means no icecream left at this level
@@ -154,7 +155,6 @@ class Player:
             get_player_count (Callable[[], int]): method which returns number of total players
             get_served (Callable[[], List[Dict[int, int]]]): method which returns a list of dictionaries corresponding to each player, each dictionary at index i tells how units of a flavor are present in the bowl of the player with index i. E.g. lets say the fourth element is {1: 0, 2: 8...} means the corresponding player with index 4 has 0 units of flavor 1 and 8 units of flavor
             get_turns_received (Callable[[], List[int]]): method which returns a list of integers corresponding to each player, each element at index i tells how many turns a player with index i has played so far.
-
         Returns:
             Dict[str, Union[Tuple[int],int]]: Return a dictionary specifying what action to take in the next step.
             2 possible return values
@@ -167,9 +167,18 @@ class Player:
 
         self.update_hidden_cell_expectation(get_served(), top_layer)
 
-        remaining_scoops = self.total_scoop_in_bowl(player_idx, get_served()) - ((self.round-1) * 24)
+        remaining_scoops = self.remaining_scoops
         if len(self.priority_moves) > 0:
             max_scoop_i, max_scoop_j = self.priority_moves.pop(0)
+            max_scoop_cells = 0
+            scoop = [[curr_level[max_scoop_i][max_scoop_j], curr_level[max_scoop_i][max_scoop_j + 1]], [curr_level[max_scoop_i + 1][max_scoop_j], curr_level[max_scoop_i + 1][max_scoop_j + 1]]]
+            max_level = np.amax(scoop)
+
+            for x in range(2):
+                for y in range(2):
+                    if curr_level[max_scoop_i+x][max_scoop_j+y] == max_level:
+                        max_scoop_cells += 1
+            
         else:
             for i in range(len(top_layer) - 1):
                 for j in range(len(top_layer[i]) - 1):
@@ -182,32 +191,46 @@ class Player:
                     #    max_scoop_i, max_scoop_j = i, j
 
             scores = list(self.move_scores.keys())
-            heapq.heapify(scores)
-            max_score = heapq.heappop(scores)
-            max_num_moves, max_scoop_cells, max_scoop_i, max_scoop_j = self.move_scores[max_score]
-            while max_scoop_cells > remaining_scoops and len(scores) > 0:
+            if len(scores) == 0:
+                time_to_pass = True
+            else:
+                heapq.heapify(scores)
                 max_score = heapq.heappop(scores)
                 max_num_moves, max_scoop_cells, max_scoop_i, max_scoop_j = self.move_scores[max_score]
+                while max_scoop_cells > remaining_scoops and len(scores) > 0:
+                    max_score = heapq.heappop(scores)
+                    max_num_moves, max_scoop_cells, max_scoop_i, max_scoop_j = self.move_scores[max_score]
 
-            if max_num_moves > 1:
-                for q in range(max_num_moves-1):
-                    self.priority_moves.append((i, j))
+                if max_num_moves > 1:
+                    max_scoop_cells = 0
+                    scoop = [[curr_level[max_scoop_i][max_scoop_j], curr_level[max_scoop_i][max_scoop_j + 1]], [curr_level[max_scoop_i + 1][max_scoop_j], curr_level[max_scoop_i + 1][max_scoop_j + 1]]]
+                    max_level = np.amax(scoop)
 
-        time_to_pass = (remaining_scoops == 24)
+                    for x in range(2):
+                        for y in range(2):
+                            if curr_level[max_scoop_i+x][max_scoop_j+y] == max_level:
+                                max_scoop_cells += 1
+                    
+                    for _ in range(max_num_moves-1):
+                        self.priority_moves.append((max_scoop_i, max_scoop_j))
 
-        if time_to_pass:
+        time_to_pass = (remaining_scoops == 0)
+        self.move_scores = {}
+
+        if time_to_pass or max_scoop_cells > remaining_scoops:
             points = []
             served = get_served()
             turns = get_turns_received()
             for idx in range(get_player_count()):
-                if idx != player_idx:
-                    point = self.calc_scr_flavor_pref(served[idx])
-                    points.append((point,turns[idx],idx))
+                point = self.calc_scr_flavor_pref(served[idx])
+                points.append((point,turns[idx],idx))
             
             sorted_points = sorted(points, key=lambda x: (x[1], x[0]))
-            next_player = sorted_points[2]
+            next_player = sorted_points[0][2]
             self.round += 1
+            self.priority_moves = []
+            self.remaining_scoops = 24
             return {"action": "pass", "values": next_player}
 
+        self.remaining_scoops -= max_scoop_cells
         return {"action": "scoop",  "values": (max_scoop_i, max_scoop_j)}
-
