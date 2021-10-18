43fd6a4128dc4b8f0258a85f4bb23ffd5486e6c1
Kerem Guventurk <kg2900@columbia.edu>
2021-10-17 20:55:17 -0400

    first attempt at optimizing last 2 scoops

--- players/g7_player.py
+++ players/g7_player.py
@@ -4,11 +4,13 @@ import copy
 import logging
 from typing import Callable, Dict, List, Tuple, Union
 import heapq
+from collections import defaultdict
 
 
 class Player:
     def __init__(self, flavor_preference: List[int], rng: np.random.Generator, logger: logging.Logger) -> None:
         """Initialise the player with given preference.
+
         Args:
             flavor_preference (List[int]): flavor preference, most flavored flavor is first element in the list and last element is least preferred flavor
             rng (np.random.Generator): numpy random number generator, use this for same player behvior across run
@@ -145,8 +147,35 @@ class Player:
             self.move_scores[-score] = (num_moves, num_scooped, i, j)
             prev_num_unseen_cells = num_unseen_cells
 
+    def calculate_last_2(self):
+        scoops_scores = defaultdict(int)
+        scoop_info = {}
+
+        for score in self.move_scores:
+            max_num_moves, max_scoop_cells, max_scoop_i, max_scoop_j = self.move_scores[score]
+
+            for s in range(1,8):
+                if s in scoop_info and s + max_scoop_cells <= self.remaining_scoops:
+                    other_s_info = scoop_info[s]
+                    if scoops_scores[s + max_scoop_cells] > scoops_scores[s] + score:
+                        scoops_scores[s + max_scoop_cells] = scoops_scores[s] * other_s_info[1] + score * max_scoop_cells
+                        if scoops_scores[s] * other_s_info[1] < score * max_scoop_cells:
+                            scoop_info[s + max_scoop_cells] = other_s_info
+                        else:
+                            scoop_info[s + max_scoop_cells] = self.move_scores[score]
+            
+            if max_scoop_cells <= self.remaining_scoops:
+                if max_scoop_cells not in scoop_info or scoops_scores[max_scoop_cells] * scoop_info[max_scoop_cells][1] > score * max_scoop_cells:
+                    scoops_scores[max_scoop_cells] = score * max_scoop_cells
+                    scoop_info[max_scoop_cells] = self.move_scores[score]
+        
+        for key,value in scoops_scores.items():
+            self.move_scores[value] = scoop_info[key]
+
+
     def serve(self, top_layer: np.ndarray, curr_level: np.ndarray, player_idx: int, get_flavors: Callable[[], List[int]], get_player_count: Callable[[], int], get_served: Callable[[], List[Dict[int, int]]], get_turns_received: Callable[[], List[int]]) -> Dict[str, Union[Tuple[int], int]]:
         """Request what to scoop or whom to pass in the given step of the turn. In each turn the simulator calls this serve function multiple times for each step for a single player, until the player has scooped 24 units of ice-cream or asked to pass to next player or made an invalid request. If you have scooped 24 units of ice-cream in a turn then you get one last step in that turn where you can specify to pass to a player.
+
         Args:
             top_layer (np.ndarray): Numpy 2d array of size (24, 15) containing flavor at each cell location
             curr_level (np.ndarray): Numpy 2d array of size (24, 15) containing current level at each cell location from 8 to 0, where 8 is highest level at start and 0 means no icecream left at this level
@@ -155,6 +184,7 @@ class Player:
             get_player_count (Callable[[], int]): method which returns number of total players
             get_served (Callable[[], List[Dict[int, int]]]): method which returns a list of dictionaries corresponding to each player, each dictionary at index i tells how units of a flavor are present in the bowl of the player with index i. E.g. lets say the fourth element is {1: 0, 2: 8...} means the corresponding player with index 4 has 0 units of flavor 1 and 8 units of flavor
             get_turns_received (Callable[[], List[int]]): method which returns a list of integers corresponding to each player, each element at index i tells how many turns a player with index i has played so far.
+
         Returns:
             Dict[str, Union[Tuple[int],int]]: Return a dictionary specifying what action to take in the next step.
             2 possible return values
@@ -183,12 +213,6 @@ class Player:
             for i in range(len(top_layer) - 1):
                 for j in range(len(top_layer[i]) - 1):
                     self.updated_score(i, j, top_layer, curr_level)
-                    #scoop = self.get_scoop(i,j,top_layer,curr_level)
-                    #scoop_point = self.calculate_score_scoop(scoop)
-
-                    #if scoop_point > max_scoop_point:
-                    #    max_scoop_point = scoop_point
-                    #    max_scoop_i, max_scoop_j = i, j
 
             scores = list(self.move_scores.keys())
             if len(scores) == 0:
