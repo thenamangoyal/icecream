81a95e3352648ebddf0a8b0c0cd365eb01bf9ffa
Naman Goyal <namangoyal1001@gmail.com>
2021-10-23 13:00:17 -0400

    Add code for tracking time taken by players

--- main.py
+++ main.py
@@ -89,6 +89,7 @@ class IceCreamGame():
         self.player_names = []
         self.player_preferences = []
         self.served = []
+        self.time_taken = []
         self.turns_received = np.zeros(0, dtype=np.int)
         self.player_scores = []
         self.next_player = -1
@@ -131,6 +132,7 @@ class IceCreamGame():
             self.player_preferences.append(player_preference)
             self.player_names.append(player_name)
             self.served.append({k: 0 for k in self.flavors})
+            self.time_taken.append([]) # a list of time taken in each turn (recorded as list of step times) for a player
             self.turns_received = np.zeros(len(self.players), dtype=np.int)
             self.player_scores.append(0)
             self.total_turn_per_player = math.floor(120 / len(self.players))
@@ -164,6 +166,17 @@ class IceCreamGame():
                 self.ice_cream_app.set_label_text("Game ended as each player played {} turns".format(self.total_turn_per_player))
             for player_idx, score in enumerate(self.player_scores):
                 self.logger.debug("{} turns: {}".format(self.player_names[player_idx], self.turns_received[player_idx]))
+            total_time = np.zeros(len(self.players))
+            for player_idx, player_time_taken in enumerate(self.time_taken):
+                player_time_taken = np.array(player_time_taken)
+                self.logger.info("{} took {} steps, total time {:.3f}s, avg step time {:.3f}s, max step time {:.3f}s".format(self.player_names[player_idx], player_time_taken.size, np.sum(player_time_taken), np.mean(player_time_taken), np.amax(player_time_taken)))
+                total_time[player_idx] = np.sum(player_time_taken)
+            self.logger.info("Total time taken by all players {:.3f}s".format(np.sum(total_time)))
+            total_time_order = np.argsort(total_time)[::-1]
+            self.logger.info("Players sorted by total time")
+            for player_idx in total_time_order:
+                self.logger.info("{} took {:.3f}s".format(self.player_names[player_idx], total_time[player_idx]))
+            
             for player_idx, player_served in enumerate(self.served):
                 self.logger.info("{} final bowl {}".format(self.player_names[player_idx],player_served))
             for player_idx, score in enumerate(self.player_scores):
@@ -244,6 +257,7 @@ class IceCreamGame():
                 self.processing_turn = True
                 self.served_this_turn = []
                 self.turns_received[self.next_player] += 1
+                self.time_taken[self.next_player].append([])
 
             else:
                 self.__game_end()
@@ -285,7 +299,10 @@ class IceCreamGame():
             top_layer = self.ice_cream_container.get_top_layer()
             curr_level = self.ice_cream_container.get_curr_level()
             try:
+                start_time = time.time()
                 action_values_dict = player.serve(top_layer, curr_level, player_idx, self.get_flavors, self.get_player_count, self.get_served, self.get_turns_received)
+                step_time = time.time() - start_time
+                self.time_taken[player_idx][-1].append(step_time)
             except Exception as e:
                 self.logger.error(e, exc_info=True)
                 action_values_dict = dict()
@@ -293,7 +310,7 @@ class IceCreamGame():
             if is_valid_action:
                 action = action_values_dict["action"]
                 values = action_values_dict["values"]
-                self.logger.debug("Received action: {} from {}".format(action_values_dict, self.player_names[player_idx]))
+                self.logger.debug("Received action: {} from {} in {:.3f}s".format(action_values_dict, self.player_names[player_idx], step_time))
                 if self.use_gui:
                     self.ice_cream_app.set_label_text("{}, {}".format(self.ice_cream_app.get_label_text(), action_values_dict))
 
