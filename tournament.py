7085c0972538691334aac4487bbff4917535db37
Naman Goyal <namangoyal1001@gmail.com>
2021-10-24 12:25:22 -0400

    Remove extra global vars and add seed entropy passing

--- tournament.py
+++ tournament.py
@@ -21,11 +21,11 @@ def generate_args(flavors, log_path, seed):
     return args
 
 def worker(worker_input):
-    global args
+    global RESULT_DIR
     family_size, player_list, flavors, trial, seed = worker_input
     # print("Running with {} size family with members {} flavors {} trial {} seed {}".format(family_size, player_list, flavors, trial, seed))
     log_path = "{}_size_family_{}_flavors_{}_trial_{}.log".format(family_size, "-".join(player_list), flavors, trial)
-    log_path = os.path.join(args.result_dir, "logs", log_path)
+    log_path = os.path.join(RESULT_DIR, "logs", log_path)
     args = generate_args(flavors=flavors, log_path=log_path, seed=seed)
     ice_cream_game = IceCreamGame(player_list=player_list, args=args)
     ice_cream_game.play_all()
@@ -39,7 +39,8 @@ if __name__ == "__main__":
     parser.add_argument("--result_dir", default="results", help="Directory path to dump results")
     parser.add_argument("--seed_entropy", "-s", type=np.uint64, help="Seed used to generate seed for each game")
     args = parser.parse_args()
-    os.makedirs(args.result_dir, exist_ok=True)
+    RESULT_DIR = args.result_dir 
+    os.makedirs(RESULT_DIR, exist_ok=True)
 
     base_tournament_configs = []
     for family_size in FAMILY_SIZES:
@@ -50,7 +51,7 @@ if __name__ == "__main__":
                     base_config = (family_size, player_list, flavors, trial)
                     base_tournament_configs.append(base_config)
     
-    seed_sequence = np.random.SeedSequence()
+    seed_sequence = np.random.SeedSequence(args.seed_entropy)
     seeds = seed_sequence.generate_state(len(base_tournament_configs), dtype=np.uint64)
 
     tournament_configs = []
@@ -58,7 +59,7 @@ if __name__ == "__main__":
         config = tuple(list(base_config) + [seeds[i]])
         tournament_configs.append(config)
 
-    out_fn = os.path.join(args.result_dir, "aggregate_results.csv")    
+    out_fn = os.path.join(RESULT_DIR, "aggregate_results.csv")    
     with open(out_fn, "w") as csvf:
         header_df = pd.DataFrame([], columns=all_df_cols)
         header_df.to_csv(csvf, index = False, header=True)
